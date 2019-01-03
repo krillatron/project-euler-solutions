33f5dc6e6eb06906a6a5112663db7cf5ae4983fc
Karen Fielding <karen@kyan.com>
2019-01-03 17:08:23 +0000

    001

--- solutions/001.rb
+++ solutions/001.rb
@@ -1,5 +1,17 @@
-def collect_multiples(amount)
+def collect_multiples(limit)
+  i = 1
+  arr = []
+
+  while i < limit
+    arr << i if ( i % 3 == 0 ) || ( i % 5 == 0 )
+    i += 1
+  end
+
+  arr
 end
 
-def sum_multiples(amount)
+def sum_multiples(limit)
+  total = 0
+  collect_multiples(limit).map{ |a| total = total + a }
+  total
 end
